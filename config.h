3b15a37cc035be0276ddc5a0d04a23fc094a1f31
Coolbrother <coolbrother@sourcegod.com>
2023-03-07 23:56:04 +0100

    Adding file: (config.h) for defining (RtAudio support, etc ...)

---
+++ config.h
@@ -0,0 +1,8 @@
+/* File configure.h 
+ * Configuring for audio drivers manager.
+ * Date: Date: Tue, 07/03/2023
+ * Author: Coolbrother
+ * */
+#define RTAUDIO_SUPPORT   1
+#define PORTAUDIO_SUPPORT 1 
+#define ALSAAUDIO_SUPPORT 1
